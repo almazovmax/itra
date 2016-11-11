4fb720c8bc4881c545ca367ae58e8f622ef0f195
almazovmax <crea.tiv@tut.by>
2016-11-11 14:31:34 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,4 @@
+itra.com
+========
+
+A Symfony project created on November 11, 2016, 2:49 pm.
---
+++ src/ItraBundle/ItraBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace ItraBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ItraBundle extends Bundle
+{
+}
