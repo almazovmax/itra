97872ac4effea1e8d338dcd1f1ba96e5a80b79dc
almazovmax <crea.tiv@tut.by>
2016-11-11 21:54:02 +0200

    reset password theEnd

--- src/ItraBundle/Controller/SecurityController.php
+++ src/ItraBundle/Controller/SecurityController.php
@@ -2,6 +2,7 @@
 namespace ItraBundle\Controller;
 
 use ItraBundle\Entity\User;
+use ItraBundle\Form\UserType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
@@ -21,6 +22,67 @@ class SecurityController extends Controller
         return $this->render('ItraBundle:Page:login.html.twig', array(
             'last_username' => $lastUsername,
             'error'         => $error,
+        ));
+    }
+
+    /**
+     * @Route("/reset_password", name="reset_password")
+     */
+    public function resetPasswordAction(Request $request)
+    {
+        $form = $this->createForm(UserType::class);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted()) {
+            $token = md5($request->getContent());
+            $email = $form['email']->getData();
+            $username = $form['username']->getData();
+            $this->getDoctrine()->getRepository('ItraBundle:User')->saveToken($token, $username);
+            $url = 'http://127.0.0.1:8000/new_password'.'?token='.$token;
+            $message = \Swift_Message::newInstance()
+                ->setSubject('Reset Password')
+                ->setFrom('itra@lol.by')
+                ->setTo($email)
+                ->setBody($this->renderView('ItraBundle:Mail:reset_password.txt.twig', array(
+                    'resetUrl' => $url,
+                    'username' => $username,
+                )));
+
+            $this->get('mailer')->send($message);
+
+            return $this->render('ItraBundle:Notice:send_link_reset_pass.html.twig');
+        }
+
+        return $this->render('ItraBundle:Page:reset_password.html.twig', array(
+            'form' => $form->createView()
+        ));
+    }
+
+
+    /**
+     * @Route("/new_password", name="new_password")
+     */
+    public function newPassswordAction(Request $request)
+    {
+        $form = $this->createForm(UserType::class);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $token=$request->get('token');
+            $em = $this->getDoctrine()->getRepository('ItraBundle:User');
+            $user = $em->findOneBy(array('token' => $token));
+            $password = $this->get('security.password_encoder')
+                ->encodePassword($user, $user->getPlainPassword());
+            $user->setPassword($password);
+
+            $em->saveNewPassword();
+            $em->removeToken($user);
+
+            return $this->redirectToRoute('login');
+        }
+
+        return $this->render('ItraBundle:Page:new_password.html.twig', array(
+            'form' => $form->createView(),
         ));
     }
 }
--- src/ItraBundle/Entity/UserRepository.php
+++ src/ItraBundle/Entity/UserRepository.php
@@ -19,4 +19,27 @@ class UserRepository extends EntityRepository implements UserLoaderInterface
     {
         return parent::findOneBy($criteria, $orderBy);
     }
+
+    public function saveToken($token, $username)
+    {
+        $user = $this->loadUserByUsername($username);
+        $user->setToken($token);
+
+        $em = $this->getEntityManager();
+        $em->flush();
+    }
+
+    public function removeToken($user)
+    {
+        $user->setToken(null);
+
+        $em = $this->getEntityManager();
+        $em->flush();
+    }
+
+    public function saveNewPassword()
+    {
+        $em= $this->getEntityManager();
+        $em->flush();
+    }
 }
