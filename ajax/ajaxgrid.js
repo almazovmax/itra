3fce2699933a90688df13cb210430d1ea52fdcc4
GrigorijGagua <grigorij.gagua@gmail.com>
2016-11-23 09:34:27 +0300

    edit of ajaxgrid
    adjusted:
    - the table generation method
    added:
    -sort by colomns(still testing)
    -filter(still testing)
    -code elements for paggination(under construction)

--- ajax/ajaxgrid.js
+++ ajax/ajaxgrid.js
@@ -3,8 +3,6 @@
  */
 
 (function( $ ) {
-    $.fn.ajaxGrid = function() {
-
         var settings = $.extend( {
             'dataUrl'          : 'http://mylocalserver/ajax/products',
             'sortableColumns'  : '[“id”, “title”, “created_date”]',
@@ -12,36 +10,89 @@
             'rowsPerPage'      : '20'
         }, options);
 
-        return this.each(function() {
-
-            $.getJSON(settings.dataUrl,function (data) {
-                function createTable(response)
-                {
-                    var c = [];
-                    for(var prop in response)
+        var methods = {
+            init : function( params ) {
+                var options = $.extend({}, settings, params);
+                $.getJSON(options.dataUrl,function (data) {
+                    function createTable(response)
+                    {
+                        var c = [];
+                        c.push("<table class='sortable'>");
+                        c.push("<thead>");
+                        for(var prop in response)
                         {
                             if(response.hasOwnProperty(prop))
-                            c.push("<th>"+prop+"</th>");
-                        }
-                    $.each(response, function(i, item)
-                        {
-                            c.push("<tr>");
-                            for(var prop in response)
                             {
-                                if(response.hasOwnProperty(prop))
-                                c.push("<td>"+response[prop]+"</td>");
+                                if(options.sortableColumns.includes(prop))
+                                    c.push("<th class='sortableColumn'>" + prop + "</th>");
+                                else
+                                    c.push("<th>" + prop + "</th>");
                             }
-                        c.push("</tr>");
                         }
-                    );
-                    return c;
+                        c.push("</thead>");
+                        c.push("<tbody id='dataRows'>");
+                        $.each(response, function()
+                            {
+                                c.push("<tr>");
+                                for(var prop in response)
+                                {
+                                    if(response.hasOwnProperty(prop))
+                                        c.push("<td>"+response[prop]+"</td>");
+                                }
+                                c.push("</tr>");
+                            }
+                        );
+                        c.push("</tbody>");
+                        c.push("</table>");
+                        // c.push("<div id='paging'>"+"Showing"+"<span id='begin'>"+"</span>"+"-"+"<span id='end'>"+
+                        // "</span>"+"of"+"<span id='size'>"+"</span>"+"<input id='last' class='btn' type='Button' value='Last' onclick='javascript:last()' />"+
+                        // "<input id='next' class='btn' type='button' value='Next' onclick='javascript:next()'/>"+"</div>");
+                        return c;
+                    }
+                    var response = $.parseJSON(data);
+                    var c=createTable(response);
+                    $(this).html(c.join(""));
+                })
+            },
+            filter:function( params ) {
+                var options = $.extend({}, settings, params);
+                $(this).keyup(function () {
+                    var rows = $("#dataRows").find("tr").hide();
+                    if (this.value.length) {
+                        var data = this.value.split(" ");
+                        $.each(data, function (i, v) {
+                            rows.filter(":contains('" + v + "')").show();
+                        });
+                    } else rows.show();
+                });
+            },
+            sort( params ) {
+                var options = $.extend({}, settings, params);
+                $('table.sortable th.sortableColumn').click(function(){
+                    var table = $(this).parents('table').eq(0);
+                    var rows = table.find('#dataRows').toArray().sort(compareParams($(this).index()));
+                    this.asc = !this.asc;
+                    if (!this.asc){rows = rows.reverse()}
+                    for (var i = 0; i < rows.length; i++){table.append(rows[i])}
+                });
+                function compareParams(index) {
+                    return function(a, b) {
+                        var valA = getCellValue(a, index), valB = getCellValue(b, index);
+                        return $.isNumeric(valA) && $.isNumeric(valB) ? valA - valB : valA.localeCompare(valB)
+                    }
                 }
-                var response = $.parseJSON(data);
-                var c=createTable(response);
-                $(this).html(c.join(""));
-            })
-        });
+                function getCellValue(row, index){ return $(row).children('td').eq(index).html() }
 
+            },
+        };
+    $.fn.ajaxGrid= function( method ) {
+        if ( methods[method] ) {
+            return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
+        } else if ( typeof method === 'object' || ! method ) {
+            return methods.init.apply( this, arguments );
+        } else {
+            $.error( 'Method named' +  method + 'does not exist for jquery.ajaxGrid');
+        }
     };
 })(jQuery);
 
