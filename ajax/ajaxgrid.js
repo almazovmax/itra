5377b2ca309d63d9cc144dae947793534c309365
GrigorijGagua <grigorij.gagua@gmail.com>
2016-11-23 15:03:56 +0300

    test changes
    testi funcional

--- ajax/ajaxgrid.js
+++ ajax/ajaxgrid.js
@@ -18,13 +18,21 @@
                     {
                         var c = [];
                         c.push("<table class='sortable'>");
-                        c.push("<thead>");
+                        c.push("<thead class='dataColumns'>");
                         for(var prop in response)
                         {
                             if(response.hasOwnProperty(prop))
                             {
-                                if(options.sortableColumns.includes(prop))
+                                if((options.filterableColumns.includes(prop))&&(options.sortableColumns.includes(prop))){
+                                    c.push("<th class='sortableColumn filterableColumns'>" + prop + "<input id='searchInput' placeholder='Type to filter'>"+ "</th>");
+                                    this.getElementById('searchInput').id=prop;
+                                }
+                                else if(options.sortableColumns.includes(prop)){
                                     c.push("<th class='sortableColumn'>" + prop + "</th>");
+                                }else if (options.filterableColumns.includes(prop)){
+                                    c.push("<th class='filterableColumns'>" + prop + "<input id='searchInput' placeholder='Type to filter'>"+"</th>");
+                                    this.getElementById('searchInput').id=prop;
+                                }
                                 else
                                     c.push("<th>" + prop + "</th>");
                             }
@@ -44,45 +52,41 @@
                         );
                         c.push("</tbody>");
                         c.push("</table>");
-                        // c.push("<div id='paging'>"+"Showing"+"<span id='begin'>"+"</span>"+"-"+"<span id='end'>"+
-                        // "</span>"+"of"+"<span id='size'>"+"</span>"+"<input id='last' class='btn' type='Button' value='Last' onclick='javascript:last()' />"+
-                        // "<input id='next' class='btn' type='button' value='Next' onclick='javascript:next()'/>"+"</div>");
+                        c.push("<div id='paging'>"+"Showing"+"<span id='begin'>"+"</span>"+"-"+"<span id='end'>"+
+                        "</span>"+"of"+"<span id='size'>"+"</span>"+"<input id='last' class='btn' type='Button' value='Last' onclick='javascript:last()' />"+
+                        "<input id='next' class='btn' type='button' value='Next' onclick='javascript:next()'/>"+"</div>");
+                        return c;
+                    }
+                    var response = $.parseJSON(data);
+                    var c=createTable(response);
+                    $(this).after().html(c.join(""));
+                })
+            },
+            update : function( params ) {
+                var options = $.extend({}, settings, params);
+                $.getJSON(options.dataUrl,function (data) {
+                    function createTable(response)
+                    {
+                        var c = [];
+                        c.push("<tbody id='dataRows'>");
+                        $.each(response, function()
+                            {
+                                c.push("<tr>");
+                                for(var prop in response)
+                                {
+                                    if(response.hasOwnProperty(prop))
+                                        c.push("<td>"+response[prop]+"</td>");
+                                }
+                                c.push("</tr>");
+                            }
+                        );
+                        c.push("</tbody>");
                         return c;
                     }
                     var response = $.parseJSON(data);
                     var c=createTable(response);
                     $(this).html(c.join(""));
                 })
-            },
-            filter:function( params ) {
-                var options = $.extend({}, settings, params);
-                $(this).keyup(function () {
-                    var rows = $("#dataRows").find("tr").hide();
-                    if (this.value.length) {
-                        var data = this.value.split(" ");
-                        $.each(data, function (i, v) {
-                            rows.filter(":contains('" + v + "')").show();
-                        });
-                    } else rows.show();
-                });
-            },
-            sort( params ) {
-                var options = $.extend({}, settings, params);
-                $('table.sortable th.sortableColumn').click(function(){
-                    var table = $(this).parents('table').eq(0);
-                    var rows = table.find('#dataRows').toArray().sort(compareParams($(this).index()));
-                    this.asc = !this.asc;
-                    if (!this.asc){rows = rows.reverse()}
-                    for (var i = 0; i < rows.length; i++){table.append(rows[i])}
-                });
-                function compareParams(index) {
-                    return function(a, b) {
-                        var valA = getCellValue(a, index), valB = getCellValue(b, index);
-                        return $.isNumeric(valA) && $.isNumeric(valB) ? valA - valB : valA.localeCompare(valB)
-                    }
-                }
-                function getCellValue(row, index){ return $(row).children('td').eq(index).html() }
-
             },
         };
     $.fn.ajaxGrid= function( method ) {
